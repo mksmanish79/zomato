c6de27ea9760eeb861a01ddd1ac2b6ab6cbc6d3b
Manish Kumar <manish.kumar@mandeeps.com>
2022-04-02 23:43:44 +0530

    new changes

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import { get } from "lodash";
+import { get, isEmpty } from "lodash";
 
 const Login = function () {
-    const [signinform, setsigninform] = useState({
-        email: '',
-        password: ''
-    });
+    const [signinform, setsigninform] = useState({ email: '', password: '' });
     const [isSignIn, setisSignIn] = useState(true);
-    const doSignIn = () => {
+    const [error, seterror] = useState({});
 
+    const validateForm = () => {
+        const _error = {};
+        if (!get(signinform, "email")) {
+            _error["email"] = "Email is required!";
+        }
+        if (!get(signinform, "password")) {
+            _error["password"] = "Password is required!";
+        }
+        seterror(_error);
+        if (isEmpty(_error))
+            return true;
+        else
+            return false;
+    };
+
+    const doSignIn = () => {
+        if (validateForm()) {
+
+        }
     };
     const doOnchange = (e, name) => {
         const _value = get(e, 'target.value');
@@ -25,10 +41,12 @@ const Login = function () {
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Email address</label>
                             <input type="email" className="form-control" id="txtloginemail" aria-describedby="emailHelp" placeholder="Enter email" value={signinform.email} onChange={e => doOnchange(e, 'email')} />
+                            {!isEmpty(get(error, "email")) ? <span style={{ color: 'red' }}>{get(error, "email")}</span> : ''}
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputPassword1">Password</label>
                             <input type="password" className="form-control" id="txtloginpassword" placeholder="Password" value={signinform.password} onChange={e => doOnchange(e, 'password')} />
+                            {get(error, 'password') && <span className="error">{get(error, 'password')}</span>}
                         </div>
                         <div className="form-group text-center">
                             {isSignIn ? <button type="button" className="btn btn-primary mt-1" onClick={doSignIn}>Sign In</button> : <button type="button" className="btn btn-primary mt-1">Sign Up</button>}
