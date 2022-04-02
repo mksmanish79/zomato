9d670bb077a99aee09d20e943c24675a33f109c1
Manish Kumar <manish.kumar@mandeeps.com>
2022-04-02 17:52:28 +0530

    new changes

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,24 +1,39 @@
 import { useState } from "react";
+import { get } from "lodash";
 
 const Login = function () {
+    const [signinform, setsigninform] = useState({
+        email: '',
+        password: ''
+    });
     const [isSignIn, setisSignIn] = useState(true);
+    const doSignIn = () => {
+
+    };
+    const doOnchange = (e, name) => {
+        const _value = get(e, 'target.value');
+        setsigninform(s => {
+            return { ...s, [name]: _value };
+        });
+    };
+
     return (
         <div className="container my-5">
             <div className="row">
                 <div className="col-md-4 offset-md-4">
                     <form>
                         <div className="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="txtloginemail" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <label htmlFor="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="txtloginemail" aria-describedby="emailHelp" placeholder="Enter email" value={signinform.email} onChange={e => doOnchange(e, 'email')} />
                         </div>
                         <div className="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="txtloginpassword" placeholder="Password" />
+                            <label htmlFor="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="txtloginpassword" placeholder="Password" value={signinform.password} onChange={e => doOnchange(e, 'password')} />
                         </div>
                         <div className="form-group text-center">
-                            {isSignIn ? <button type="button" className="btn btn-primary mt-1">Sign In</button> : <button type="button" className="btn btn-primary mt-1">Sign Up</button>}
+                            {isSignIn ? <button type="button" className="btn btn-primary mt-1" onClick={doSignIn}>Sign In</button> : <button type="button" className="btn btn-primary mt-1">Sign Up</button>}
                             <hr />
-                            {isSignIn ? <button type="button" className="btn mt-1" onClick={()=>setisSignIn(false)}>Sign Up</button> : <button type="button" className="btn mt-1" onClick={()=>setisSignIn(true)}>Sign In</button>}
+                            {isSignIn ? <button type="button" className="btn mt-1" onClick={() => setisSignIn(false)}>Sign Up</button> : <button type="button" className="btn mt-1" onClick={() => setisSignIn(true)}>Sign In</button>}
                         </div>
                     </form>
                 </div>
