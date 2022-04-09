aae144e191a20c73f7e61cf36438eec90caea013
Manish Kumar <manish.kumar@mandeeps.com>
2022-04-09 16:42:15 +0530

    added swagger

--- API/zomato/Startup.cs
+++ API/zomato/Startup.cs
@@ -26,6 +26,7 @@ namespace zomato
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@ namespace zomato
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zomato v1"));
             }
 
             app.UseHttpsRedirection();
